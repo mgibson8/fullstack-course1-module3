cc90fc2d4daeffb62f58383fc9c741874c528647
Matt Gibson <matt@mpgibson.com>
2015-10-27 11:13:43 +0000

    Completed.

--- Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
+++ Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
@@ -1,5 +1,13 @@
 require 'httparty'
 
 class Recipe
+  include HTTParty
 
+  base_uri "http://food2fork.com/api"
+  default_params key: ENV["FOOD2FORK_KEY"] = "2ac1966ebcce9f892a76613e0b8c26aa"
+  format :json
+
+  def self.for (keyword)
+    get("/search", query: {q: keyword})["recipes"]
+  end
 end
